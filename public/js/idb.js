05f422d4bad7df50207d8c5663e950fd7abde5f7
Jason Ahmed <jason_ahmed@yahoo.com>
2020-09-24 13:13:48 -0700

    updated IndexedDB file for tracking offline transactions

--- public/js/idb.js
+++ public/js/idb.js
@@ -29,3 +29,46 @@ request.onsuccess = function(event) {
     // log error here
     console.log("Sorry!" + event.target.errorCode);
   };
+
+  // function that is called in index.js when user is offline:
+  function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+  
+    store.add(record);
+  }
+  
+  // function that is called when user goes back online:
+  function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+  
+    getAll.onsuccess = function () {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+          .then(response => response.json())
+          .then(() => {
+            // delete records if successful
+            const transaction = db.transaction(["pending"], "readwrite");
+            const store = transaction.objectStore("pending");
+            store.clear();
+          });
+      }
+    };
+  }
+  function deletePending() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.clear();
+  }
+  
+  // listen for when coming back online:
+  window.addEventListener("online", checkDatabase);
