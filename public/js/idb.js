24c45b540796b3e44f7677f4d5d73f35c19a0b02
Jason Ahmed <jason_ahmed@yahoo.com>
2020-09-24 16:54:48 -0700

    fixed pending_transactions in indexDB

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,47 +6,49 @@ let db;
 const request = indexedDB.open('budget_tracker', 1);
 
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
-request.onupgradeneeded = function(event) {
-    // save a reference to the database 
-    const db = event.target.result;
-    // create an object store (table) called `pending_transaction`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('pending_transaction', { autoIncrement: true });
-  };
+request.onupgradeneeded = function (event) {
+  // save a reference to the database 
+  const db = event.target.result;
+  // create an object store (table) called `pending_transaction`, set it to have an auto incrementing primary key of sorts 
+  db.createObjectStore('pending_transaction', {
+    autoIncrement: true
+  });
+};
 
 
-  // upon a successful 
-request.onsuccess = function(event) {
-    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
-    db = event.target.result;
-  
-    // check if app is online, if yes run uploadPizza() function to send all local db data to api
-    if (navigator.onLine) {
-      checkDatabase();
-    }
-  };
-  
-  request.onerror = function(event) {
-    // log error here
-    console.log("Sorry!" + event.target.errorCode);
-  };
+// upon a successful 
+request.onsuccess = function (event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+  db = event.target.result;
 
-  // function that is called in index.js when user is offline:
-  function saveRecord(record) {
-    const transaction = db.transaction(["pending"], "readwrite");
-    const store = transaction.objectStore("pending");
-  
-    store.add(record);
+  // check if app is online, if yes run uploadPizza() function to send all local db data to api
+  if (navigator.onLine) {
+    checkDatabase();
   }
-  
-  // function that is called when user goes back online:
-  function checkDatabase() {
-    const transaction = db.transaction(["pending"], "readwrite");
-    const store = transaction.objectStore("pending");
-    const getAll = store.getAll();
-  
-    getAll.onsuccess = function () {
-      if (getAll.result.length > 0) {
-        fetch("/api/transaction/bulk", {
+};
+
+request.onerror = function (event) {
+  // log error here
+  console.log("Sorry!" + event.target.errorCode);
+};
+
+// function that is called in index.js when user is offline:
+function saveRecord(record) {
+  const transaction = db.transaction(["pending_transaction"], "readwrite");
+  const store = transaction.objectStore("pending_transaction");
+
+  store.add(record);
+}
+
+// function that is called when user goes back online:
+function checkDatabase() {
+  const transaction = db.transaction(["pending_transaction"], "readwrite");
+  const store = transaction.objectStore("pending_transaction");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
           method: "POST",
           body: JSON.stringify(getAll.result),
           headers: {
@@ -54,21 +56,22 @@ request.onsuccess = function(event) {
             "Content-Type": "application/json"
           }
         })
-          .then(response => response.json())
-          .then(() => {
-            // delete records if successful
-            const transaction = db.transaction(["pending"], "readwrite");
-            const store = transaction.objectStore("pending");
-            store.clear();
-          });
-      }
-    };
-  }
-  function deletePending() {
-    const transaction = db.transaction(["pending"], "readwrite");
-    const store = transaction.objectStore("pending");
-    store.clear();
-  }
-  
-  // listen for when coming back online:
-  window.addEventListener("online", checkDatabase);
+        .then(response => response.json())
+        .then(() => {
+          // delete records if successful
+          const transaction = db.transaction(["pending_transaction"], "readwrite");
+          const store = transaction.objectStore("pending_transaction");
+          store.clear();
+        });
+    }
+  };
+}
+
+function deletePending() {
+  const transaction = db.transaction(["pending_transaction"], "readwrite");
+  const store = transaction.objectStore("pending_transaction");
+  store.clear();
+}
+
+// listen for when coming back online:
+window.addEventListener("online", checkDatabase);
