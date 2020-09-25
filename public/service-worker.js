6e380fcf1db6ffc8639e92a78f26807974cce201
Jason Ahmed <jason_ahmed@yahoo.com>
2020-09-24 17:02:42 -0700

    service-worker FILES_TO_CACHE updated

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,7 @@ const FILES_TO_CACHE = [
   "./css/style.css",
   "./js/index.js",
   "./js/idb.js",
-  "./manifest.webmanifest"
+  "./manifest.webmanifest",
   "./icons/icon-72x72.png",
   "./icons/icon-96x96.png",
   "./icons/icon-128x128.png",
